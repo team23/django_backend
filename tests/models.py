7cc611007fb42c7cf43d5f7de0768d3ab391bef8
Gregor Müllegger <gregor@muellegger.de>
2015-09-09 11:44:03 +0200

    Add basic BaseBackendForm tests

--- tests/models.py
+++ tests/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class PermissionTestModel(models.Model):
     pass
+
+
+class OneFieldModel(models.Model):
+    name = models.CharField(max_length=50)
---
+++ tests/test_backend_forms.py
@@ -0,0 +1,31 @@
+import floppyforms.__future__ as floppyforms
+from django.test import TestCase
+
+from django_backend.backend.base.forms import BaseBackendForm
+
+from .models import OneFieldModel
+
+
+class OneFieldForm(BaseBackendForm):
+    class Meta:
+        model = OneFieldModel
+        exclude = ()
+
+
+class BaseBackendFormTests(TestCase):
+    def test_has_superform_metaclass(self):
+        from django_superform.forms import SuperModelFormMetaclass
+
+        self.assertTrue(
+            issubclass(BaseBackendForm.__metaclass__, SuperModelFormMetaclass))
+
+    def test_has_floppyforms_metaclass(self):
+        from floppyforms.__future__.models import ModelFormMetaclass
+
+        self.assertTrue(
+            issubclass(BaseBackendForm.__metaclass__, ModelFormMetaclass))
+
+    def test_model_field_is_using_floppyforms_widget(self):
+        form = OneFieldForm()
+        self.assertTrue(
+            isinstance(form.fields['name'].widget, floppyforms.TextInput))
