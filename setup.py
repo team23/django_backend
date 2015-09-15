3e2e5d124e979e8c21df574cd075e05bd3e7a445
Gregor Müllegger <gregor@muellegger.de>
2015-09-15 11:55:22 +0200

    Fix find_packages in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
     version=find_version('django_backend', '__init__.py'),
     author=u'David Danier',
     author_email='david.danier@team23.de',
-    packages=find_packages('django_backend/*'),
+    packages=find_packages(
+        exclude=['examples', 'examples.*', 'tests', 'tests.*']),
     include_package_data=True,
     url='https://github.com/team23/django-backend',
     license='BSD licence, see LICENSE file',
