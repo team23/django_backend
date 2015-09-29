f51a4727c6327e5fedaed948a68fe513b93967fd
Gregor Müllegger <gregor@muellegger.de>
2015-09-29 15:03:30 +0200

    Remove assets from test settings

--- tests/settings.py
+++ tests/settings.py
@@ -16,7 +16,6 @@ USE_L10N = True
 INSTALLED_APPS = [
     'django_backend',
     'django_ajax',
-    'django_assets',
     'django_callable_perms',
     'django_superform',
     'django_viewset',
@@ -30,7 +29,6 @@ INSTALLED_APPS = [
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django_assets.finders.AssetsFinder',
 )
 
 MIDDLEWARE_CLASSES = ()
