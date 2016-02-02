0ed74fd150f6d48cae5f32b5b3d83b3635febab8
David Danier <danier@team23.de>
2016-02-02 11:02:24 +0100

    Preparing 0.3.0

--- django_backend/__init__.py
+++ django_backend/__init__.py
@@ -3,7 +3,7 @@ from .group import Group  # noqa
 from .sitebackend import SiteBackend
 
 
-__version__ = '0.2.0.dev4'
+__version__ = '0.3.0'
 
 
 default_app_config = 'django_backend.apps.DjangoBackendConfig'
