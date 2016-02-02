913e07a06f64fa9142b1a382e69a65f4a45547b7
David Danier <danier@team23.de>
2016-02-02 11:03:38 +0100

    Release 0.3.0 done

--- django_backend/__init__.py
+++ django_backend/__init__.py
@@ -3,7 +3,7 @@ from .group import Group  # noqa
 from .sitebackend import SiteBackend
 
 
-__version__ = '0.3.0'
+__version__ = '0.3.1.dev1'
 
 
 default_app_config = 'django_backend.apps.DjangoBackendConfig'
