f999bc2022ac8244537498cfd91bac9c27c952c9
Gregor Müllegger <gregor@muellegger.de>
2016-02-19 12:01:09 +0100

    Detect actual field names for GenericForeignKey in GenericRelationListField
    Before we just guessed, which was not very robust.

--- django_backend/forms/relation_list_fields/generic/fields.py
+++ django_backend/forms/relation_list_fields/generic/fields.py
@@ -1,4 +1,5 @@
 import floppyforms.__future__ as forms
+from django.contrib.contenttypes.generic import GenericForeignKey
 
 from ..base import BaseRelationListField
 from .widgets import GenericRelationListWidget
@@ -35,5 +36,18 @@ class GenericRelationListField(BaseRelationListField):
             model,
             kwargs.get('order_field', None)))
         super(GenericRelationListField, self).__init__(*args, **kwargs)
-        self.widget.set_object_id_field_name('pk')
-        self.widget.set_content_type_field_name('content_type')
+
+        self.determine_generic_relation_fields(model)
+
+    def determine_generic_relation_fields(self, model):
+        generic_fks = [
+            field
+            for field in model._meta.get_fields()
+            if isinstance(field, GenericForeignKey)]
+
+        assert len(generic_fks) == 1, (
+            'Given model {model} requires exactly one GenericForeignKey.')
+
+        generic_fk = generic_fks[0]
+        self.widget.set_object_id_field_name(generic_fk.fk_field)
+        self.widget.set_content_type_field_name(generic_fk.ct_field)
