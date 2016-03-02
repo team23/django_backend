c47c1672faaf8894c36f1cb7619637b4d668393d
Gregor Müllegger <gregor@muellegger.de>
2016-03-02 10:15:43 +0100

    Validate content type against allowed related models in GenericRelationListField

--- django_backend/forms/relation_list_fields/generic/fields.py
+++ django_backend/forms/relation_list_fields/generic/fields.py
@@ -1,5 +1,7 @@
 import floppyforms.__future__ as forms
 from django.contrib.contenttypes.generic import GenericForeignKey
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
 
 from ..base import BaseRelationListField
 from .widgets import GenericRelationListWidget
@@ -8,18 +10,32 @@ from .widgets import GenericRelationListWidget
 __all__ = ('GenericRelationListField',)
 
 
-def get_default_form(model, order_field):
-    form_fields = (
-        'content_type',
-        'object_id',
-    )
+def get_default_form(model, order_field, related_models):
+    ct_field = 'content_type'
+    fk_field = 'object_id'
 
+    form_fields = (ct_field, fk_field)
     if order_field:
         form_fields = (order_field,) + form_fields
 
     class GenericRelationListForm(forms.ModelForm):
         class Meta:
             fields = form_fields
+
+        def clean(self):
+            cleaned_data = super(GenericRelationListForm, self).clean()
+            content_type = cleaned_data.get(ct_field)
+            if content_type:
+                if content_type.model_class() not in related_models:
+                    self.add_error(field=ct_field, error=_(
+                        'The selected type {type} is not allowed here. '
+                        'Choose one of the following: {allowed}'.format(
+                            type=content_type,
+                            allowed=', '.join(
+                                force_text(model._meta.verbose_name)
+                                for model in related_models))
+                    ))
+            return cleaned_data
 
     return GenericRelationListForm
 
@@ -34,7 +50,8 @@ class GenericRelationListField(BaseRelationListField):
         model = kwargs['model']
         kwargs.setdefault('form', get_default_form(
             model,
-            kwargs.get('order_field', None)))
+            kwargs.get('order_field', None),
+            related_models=kwargs['related_models']))
         super(GenericRelationListField, self).__init__(*args, **kwargs)
 
         self.determine_generic_relation_fields(model)
